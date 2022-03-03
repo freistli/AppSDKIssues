8381d8c6b21d3d4b0ad1bf15676809af3a277294
Freist Li <freistli@microsoft.com>
2022-03-03 21:57:40 +0800

    prepare datagrid print task

--- AppSDKIssues/DataGridPageControl.xaml.cs
+++ AppSDKIssues/DataGridPageControl.xaml.cs
@@ -1,20 +1,25 @@
 ﻿using AppSDKIssues.Model;
 using CommunityToolkit.WinUI.UI;
 using CommunityToolkit.WinUI.UI.Controls;
+using Microsoft.Graphics.Canvas;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Imaging;
 using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Printing;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -156,5 +161,12 @@ namespace AppSDKIssues
         {
 
         }
+
+        private async void printButton_Click(object sender, RoutedEventArgs e)
+        {
+            var renderBitmap = new RenderTargetBitmap();
+            await renderBitmap.RenderAsync(dataGrid);
+        }
+ 
     }
 }
